c56a58b51d7fc20f080f55f07a3dd50810832c86
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2026-01-04 06:54:03 +0530

    saved

---
+++ app/login-as-harvester/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { BsArrowLeft } from "react-icons/bs";
+import { FiEye, FiEyeOff, FiLock, FiMail } from "react-icons/fi";
+import { PiEggThin } from "react-icons/pi";
+
+const Login = () => {
+  const [textVisual, setTextVisual] = useState(false);
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-br from-green-200 to-green-700 font-sans text-gray-900 text-sm p-5">
+      <div className="font-bold w-full text-4xl flex flex-row gap-2 justify-start items-center text-green-400">
+        <PiEggThin/>
+        Coconut
+      </div>
+      <div className="flex flex-col justify-center items-center w-1/4 gap-5 bg-white/40 p-5 rounded-xl">
+        <h1 className="font-bold w-full text-left text-2xl">
+          Sign in to your account
+        </h1>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiMail />
+          <input
+            placeholder="Enter the email"
+            type="email"
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+        </div>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiLock />
+          <input
+            placeholder="Enter the password"
+            type={textVisual ? "text" : "password"}
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+          {textVisual ? (
+            <FiEye
+              onClick={() => {
+                setTextVisual(false);
+              }}
+            />
+          ) : (
+            <FiEyeOff
+              onClick={() => {
+                setTextVisual(true);
+              }}
+            />
+          )}
+        </div>
+        <Link href="/forgot-password" className="w-full text-right">
+          Forgot password?
+        </Link>
+        <button className="bg-gradient-to-r from-green-400 to-green-700 text-white p-2 rounded-sm w-full text-center cursor-pointer transition duration-300 ease-in-out hover:from-green-500 hover:to-green-800">
+          SIGN IN
+        </button>
+        <label className="w-full flex flex-row gap-1 justify-center items-center">
+          New to Coconut? 
+          <Link href="/register-as-harvester" className="text-green-600 font-bold">
+            Register
+          </Link>
+        </label>
+        <Link
+          href="/"
+          className="w-full flex flex-row justify-center items-center gap-2 text-green-600"
+        >
+          <BsArrowLeft /> Back to main
+        </Link>
+      </div>
+      <label className="w-full text-center">@2026 Coconut Inc.</label>
+    </div>
+  );
+};
+
+export default Login;
---
+++ app/register-as-field-owner/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { BsArrowLeft } from "react-icons/bs";
+import {
+    FiCamera,
+  FiEye,
+  FiEyeOff,
+  FiLock,
+  FiMail,
+  FiPhoneCall,
+  FiUser,
+} from "react-icons/fi";
+import { PiEggThin } from "react-icons/pi";
+
+const Register = () => {
+  const [textVisual1, setTextVisual1] = useState(false);
+  const [textVisual2, setTextVisual2] = useState(false);
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-br from-green-200 to-green-700 font-sans text-gray-900 text-sm p-5">
+      <div className="font-bold w-full text-4xl flex flex-row gap-2 justify-start items-center text-green-400">
+        <PiEggThin />
+        Coconut
+      </div>
+      <div className="flex flex-col justify-center items-center w-1/4 gap-5 bg-white/40 p-5 rounded-xl">
+        <h1 className="font-bold w-full text-left text-2xl">
+          Sign up to your account
+        </h1>
+        <div className="flex flex-col justify-center items-center">
+        <div className="bg-white p-5 rounded-full"><FiCamera/></div>Add profile</div>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiUser />
+          <input
+          type="text"
+            placeholder="Enter the username"
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+        </div>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiPhoneCall />
+          <input
+          type="tel"
+            placeholder="Enter the phone number"
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+        </div>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiMail />
+          <input
+          type="email"
+            placeholder="Enter the email"
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+        </div>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiLock />
+          <input
+            placeholder="Enter the new password"
+            type={textVisual1 ? "text" : "password"}
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+          {textVisual1 ? (
+            <FiEye
+              onClick={() => {
+                setTextVisual1(false);
+              }}
+            />
+          ) : (
+            <FiEyeOff
+              onClick={() => {
+                setTextVisual1(true);
+              }}
+            />
+          )}
+        </div>
+        <div className="flex flex-row gap-2 justify-start items-center bg-white p-2 rounded-sm w-full">
+          <FiLock />
+          <input
+            placeholder="Enter the confirm password"
+            type={textVisual2 ? "text" : "password"}
+            className="w-full focus:outline-none focus:ring-0 border-none"
+          />
+          {textVisual2 ? (
+            <FiEye
+              onClick={() => {
+                setTextVisual2(false);
+              }}
+            />
+          ) : (
+            <FiEyeOff
+              onClick={() => {
+                setTextVisual2(true);
+              }}
+            />
+          )}
+        </div>
+        <button className="bg-gradient-to-r from-green-400 to-green-700 text-white p-2 rounded-sm w-full text-center cursor-pointer transition duration-300 ease-in-out hover:from-green-500 hover:to-green-800">
+          SIGN UP
+        </button>
+        <Link
+          href="/login-as-field-owner"
+          className="w-full flex flex-row justify-center items-center gap-2 text-green-600"
+        >
+          <BsArrowLeft /> Back to login
+        </Link>
+      </div>
+      <label className="w-full text-center">@2026 Coconut Inc.</label>
+    </div>
+  );
+};
+
+export default Register;
