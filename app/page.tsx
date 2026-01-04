1b52beaa04e7801b553bb3e30e54b78e6f939654
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2026-01-04 07:12:01 +0530

    saved

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans">
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans text-gray-900">
          <Link href="/login-as-harvester">
          continue as havester
          </Link>
